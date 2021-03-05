90fb5807813e359b97568012f8c8dd2bc86298a1
Brian K. Vagnini <bkvagnini@gmail.com>
2021-03-05 09:37:14 -0500

    Add routes.

--- demo.py
+++ demo.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/', methods = ['GET'])
-
 def home():
     return render_template('index.html')
+
+@app.route('/about', methods = ['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods = ['GET'])
+def contact():
+    return render_template('contact.html')
 
 if __name__ =='__main__':
     app.run(port = 7000, debug = True)
