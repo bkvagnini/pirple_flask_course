31d9e391673c01725ad07785724b378d4b2dfdd8
Brian K. Vagnini <bkvagnini@gmail.com>
2021-03-08 16:36:01 -0500

    Create Basic Login.

--- demo.py
+++ demo.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        #this will normally go into a db
+        if username == "Gordon" and password == "Ramsay":
+            return render_template('index.html', message = 'Login Successful')
+        else:
+            error_message = 'Hint: He curses a lot.'
+            return render_template('index.html', message = error_message)
 
 @app.route('/about', methods = ['GET'])
 def about():
