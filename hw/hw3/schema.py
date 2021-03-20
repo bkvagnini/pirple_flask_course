d63f9e8689b276333db8f48249c1cffa595161e6
Brian K. Vagnini <bkvagnini@gmail.com>
2021-03-20 10:15:52 -0400

    Add HW3.

--- hw/hw3/schema.py
+++ hw/hw3/schema.py
@@ -5,10 +5,17 @@ connection = sqlite3.connect('ToDo.db', check_same_thread = False)
 cursor = connection.cursor()
 cursor.execute(
     """  CREATE TABLE users (
-        pk Integer Primary Key AutoIncrement,
+        user_pk Integer Primary Key AutoIncrement,
         username VARCHAR(16),
         password VARCHAR(32),
         task VARCHAR(32)
+   ); 
+
+      CREATE TABLE tasks (
+        task_pk Integer Primary Key AutoIncrement,
+        user_pk Integer,
+        item VARCHAR(16),
+        status VARCHAR(32)
     ); """
 )
 
